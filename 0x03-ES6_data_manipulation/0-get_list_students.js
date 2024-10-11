5dd42e0ba5faac2b0b51ab8a97850e32377e1a72
pouch <poucheuwilfried1@gmail.com>
2024-10-11 08:12:54 -0700

    first commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+// export default
+function getListStudents() {
+  const obj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return obj;
+}
+console.log(getListStudents());
