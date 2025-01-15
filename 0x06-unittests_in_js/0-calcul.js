cd6f61a50c6095a0bec796093ab6a0088d927599
pouchkine <pouchkinecamerdouala@gmail.com>
2025-01-16 01:48:58 +0200

    0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,3 @@
-/**
- * Round params and sum them
- * @param {number} a
- * @param {number} b
- */
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,83 +1,36 @@
-/**
- * Test calculateNumber function
- */
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe("calculateNumber", function() {
-  it("should return 0 with 0.1 and 0.4", function() {
-    assert.equal(calculateNumber(0.1, 0.4), 0);
+describe('calculateNumber', () => {
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
-  it("should return 1 with 0.1 and 0.5", function() {
-    assert.equal(calculateNumber(0.1, 0.5), 1);
+
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
-  it("should return 4 with 1 and 3", function() {
-    assert.equal(calculateNumber(1, 3), 4);
+
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
-  it("should return 5 with 1 and 3.7", function() {
-    assert.equal(calculateNumber(1, 3.7), 5);
+
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
-  it("should return 5 with 1.4 and 3.7", function() {
-    assert.equal(calculateNumber(1.4, 3.7), 5);
+
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
   });
-  it("should return 6 with 1.5 and 3.7", function() {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
   });
-  it("should return 0 with -0.1 and 0.4", function() {
-    assert.equal(calculateNumber(-0.1, 0.4), 0);
+
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
   });
-  it("should return 0 with -0.1 and -0.4", function() {
-    assert.equal(calculateNumber(-0.1, -0.4), 0);
+
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
-  it("should return 0 with -0.1 and -0.5", function() {
-    assert.equal(calculateNumber(-0.1, -0.5), 0);
-  });
-  it("should return -1 with -1.5 and -0.5", function() {
-    assert.equal(calculateNumber(-1.5, -0.5), -1);
-  });
-  it("should return -3 with -1.6 and -0.6", function() {
-    assert.equal(calculateNumber(-1.6, -0.6), -3);
-  });
-  it("should return 0 with -1.1 and 1.1", function() {
-    assert.equal(calculateNumber(-1.1, 1.1), 0);
-  });
-  it("should return -4 with -1 and -3", function() {
-    assert.equal(calculateNumber(-1, -3), -4);
-  });
-  it("should return 3 with -1 and 3.7", function() {
-    assert.equal(calculateNumber(-1, 3.7), 3);
-  });
-  it("should return -3 with 1 and -3.7", function() {
-    assert.equal(calculateNumber(1, -3.7), -3);
-  });
-  it("should return -5 with -1 and -3.7", function() {
-    assert.equal(calculateNumber(-1, -3.7), -5);
-  });
-  it("should return -5 with -1.4 and -3.7", function() {
-    assert.equal(calculateNumber(-1.4, -3.7), -5);
-  });
-  it("should return 3 with -1.4 and 3.7", function() {
-    assert.equal(calculateNumber(-1.4, 3.7), 3);
-  });
-  it("should return -3 with -1.4 and -3.7", function() {
-    assert.equal(calculateNumber(1.4, -3.7), -3);
-  });
-  it("should return -5 with -1.5 and -3.7", function() {
-    assert.equal(calculateNumber(-1.5, -3.7), -5);
-  });
-  it("should return -2 with 1.5 and -3.7", function() {
-    assert.equal(calculateNumber(1.5, -3.7), -2);
-  });
-  it("should return 3 with -1.5 and 3.7", function() {
-    assert.equal(calculateNumber(-1.5, 3.7), 3);
-  });
-  it("should return -6 with -1.6 and -3.7", function() {
-    assert.equal(calculateNumber(-1.6, -3.7), -6);
-  });
-  it("should return 2 with -1.6 and 3.7", function() {
-    assert.equal(calculateNumber(-1.6, 3.7), 2);
-  });
-  it("should return 2 with -1.6 and 3.7", function() {
-    assert.equal(calculateNumber(-1.6, 3.7), 2);
-  });
-})
+});
